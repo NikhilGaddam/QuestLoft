1e6c4c953901fcd029a6a752d6dfe885d97eac0e
NikhilGaddam <nikhilgaddam@vt.edu>
2024-10-24 01:47:44 -0400

    Update schema

--- init.sql
+++ init.sql
@@ -1,8 +1,38 @@
-CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Users (
+  UserID SERIAL PRIMARY KEY,
   auth0_user_id VARCHAR(255),
-  user_role VARCHAR(50),
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  user_role VARCHAR(50) CHECK (user_role IN ('Student', 'Teacher', 'Parent', 'Admin')),
   is_approved BOOLEAN DEFAULT FALSE,
   user_created_time TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   approved_time TIMESTAMP WITHOUT TIME ZONE
+);
+
+CREATE TABLE IF NOT EXISTS Students (
+  StudentID SERIAL PRIMARY KEY,
+  UserID INT,
+  Grade VARCHAR(10),
+  FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Parents (
+  ParentID SERIAL PRIMARY KEY,
+  UserID INT,
+  StudentUserID INT,
+  FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE CASCADE,
+  FOREIGN KEY (StudentUserID) REFERENCES Students(StudentID) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS TestScores (
+  TestID SERIAL PRIMARY KEY,
+  UserID INT,
+  Score INT,
+  TotalQuestions INT,
+  CorrectAnswers INT,
+  IncorrectAnswers INT,
+  AreasWellDone TEXT,
+  AreasToImprove TEXT,
+  TestDate TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE CASCADE
 );
